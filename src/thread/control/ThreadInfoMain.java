9155dc3d5ecf65f7da1d92e555e700a8d6bfca95
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2025-04-21 22:32:38 +0900

    threadInfo

---
+++ src/thread/control/ThreadInfoMain.java
@@ -0,0 +1,26 @@
+package thread.control;
+
+import thread.start.HelloRunnable;
+
+import static util.MyLogger.log;
+
+public class ThreadInfoMain {
+    public static void main(String[] args) {
+        // main 스레드
+        Thread mainThread = Thread.currentThread();
+        log("mainThread = " + mainThread);
+        log("mainThread.threadId() = " + mainThread.threadId()); // 자동으로 만드는 것, 절대로 중복되지 않음.
+        log("mainThread.getName() = " + mainThread.getName());
+        log("mainThread.getPriority() = " + mainThread.getPriority()); // 1 ~ 10 (기본값 5)
+        log("mainThread.getThreadGroup() = " + mainThread.getThreadGroup());
+        log("mainThread.getState() = " + mainThread.getState());
+
+        Thread myThread = new Thread(new HelloRunnable(), "myThread");
+        log("myThread = " + myThread);
+        log("myThread.threadId() = " + myThread.threadId());
+        log("myThread.getName() = " + myThread.getName());
+        log("myThread.getPriority() = " + myThread.getPriority());
+        log("myThread.getThreadGroup() = " + myThread.getThreadGroup());
+        log("myThread.getState() = " + myThread.getState());
+    }
+}
